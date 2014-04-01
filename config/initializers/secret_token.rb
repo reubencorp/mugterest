d59a41641360c22227899caf1db30407c6204d18
Davidson Reuben <rubyrailseng86@gmail.com>
2014-03-31 20:16:18 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mugterest::Application.config.secret_key_base = 'bdef600547ea0ce8f6c39add416de69fb3851e31b7b4884b8a9a7189524cef74508a61a3db2eb6e005c9b8a1610439486c57daac439ee9daa457d38a246b3ed6'
